a5f7790c4d38c0197ab578c867a960c5a7caf9c4
Michael Chandra <michael57986@gmail.com>
2024-09-28 12:08:23 -0700

    Account model name changed

--- backend/src/main/java/com/backend/sb/models/Account.java
+++ backend/src/main/java/com/backend/sb/models/Account.java
@@ -2,29 +2,23 @@ package com.backend.sb.models;
 import jakarta.persistence.*;
 
 @Entity
-<<<<<<< HEAD
-@Table(name = "Accounts")
-public class Accounts {
-=======
 @Table(name = "Account")
 public class Account {
->>>>>>> 612317b98218b7a88ec035a553bd188a054fa404
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int accountID;
 
     // Attributes
-<<<<<<< HEAD
     private String name;
     private String email;
     private String password;
 
     // Default constructor 
-    public Accounts() {
+    public Account() {
     }
 
     // Parameterized constructor
-    public Accounts(String name, String email, String password) {
+    public Account(String name, String email, String password) {
         this.name = name;
         this.email = email;
         this.password = password;
@@ -55,6 +49,4 @@ public class Account {
     public void setPassword(String password) {
         this.password = password;
     }
-=======
->>>>>>> 612317b98218b7a88ec035a553bd188a054fa404
 }
