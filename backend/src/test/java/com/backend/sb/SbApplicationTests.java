3308baf095e0cf85d152c8f6fcabf75b02ac7d7a
Andy <andyhuang9338@gmail.com>
2024-09-28 09:40:17 -0700

    initialized react and springboot

---
+++ backend/src/test/java/com/backend/sb/SbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
