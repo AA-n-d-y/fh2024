315a7f36c7628a66f0d04af7dec99502914b9437
asdfzxcvEL <134179483+asdfzxcvEL@users.noreply.github.com>
2024-09-28 14:28:59 -0700

    updated design of home

--- frontend/src/Navbar.js
+++ frontend/src/Navbar.js
@@ -1,23 +1,27 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPersonRunning } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
 
     return(
-    <nav>
-        <ul class="navbar navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Teleportal</a>
-            <li class="nav-item">
-                <a href="#">HOME</a>
-            </li>
-            <li class="nav-item">
-                <a href="#">PLANETS</a>
-            </li>
-            <li>
-                <a href="#" class="btn btn-primary">SIGN UP</a>
-            </li>
-            <li>
-                <a href="#" class="btn btn-primary">LOGIN</a>
-            </li>
-        </ul>
+    <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <div className="container">
+        <div>
+            <FontAwesomeIcon icon={faPersonRunning} className="nav-item pe-2" color="white" size="lg"/>
+            <a className="navbar-brand" href="#">Teleportal</a>
+        </div>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link" href="#" aria-current="page">HOME</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">PAYMENTS</a>
+                </li>
+                <li>
+                    <a href="#" className="nav-link">LOGIN</a>
+                </li>
+            </ul>
+        </div>
     </nav>
     )
 }
