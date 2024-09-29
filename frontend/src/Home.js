85647915ef3f3c90b4ff2c9bf370ec9e785b5cea
asdfzxcvEL <134179483+asdfzxcvEL@users.noreply.github.com>
2024-09-28 17:43:29 -0700

    updated Home.js

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -16,17 +16,26 @@ function Home() {
                 </div>
             </div>
         </section>
-        <section className="section-two">
+        <section className="section-two fade2">
             <div className="container">
                 <img className="img-1" src="https://placehold.co/400"></img>
-                <p>Introducing Galactic Gateway, a pioneering company revolutionizing space travel with our state-of-the-art portal system. We provide a unique solution that allows users to traverse the cosmos effortlessly by spawning portals directly at their location. Our advanced technology enables individuals to step through these portals and instantly arrive at their desired planetary destination, transforming the way we explore the universe. With a commitment to safety and innovation, Galactic Gateway opens up a realm of possibilities, making interplanetary travel accessible and exciting for everyone. Experience the future of travel with us, where the universe is just a step away.</p>
+                <p className="description">Introducing Galactic Gateway, a pioneering company revolutionizing space travel with our state-of-the-art portal system. We provide a unique solution that allows users to traverse the cosmos effortlessly by spawning portals directly at their location. Our advanced technology enables individuals to step through these portals and instantly arrive at their desired planetary destination, transforming the way we explore the universe. With a commitment to safety and innovation, Galactic Gateway opens up a realm of possibilities, making interplanetary travel accessible and exciting for everyone. Experience the future of travel with us, where the universe is just a step away.</p>
             </div>
         </section>
-        <section className="section-three">
+        <section className="section-three fade2">
             <div className="container">
                 <img className="img-2" src="https://placehold.co/400"></img>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <p className="description2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             </div>
+        </section>
+        <section>
+            <h2>Customer Reviews</h2>
+            <div className="reviews">
+                <Card name="edmon" review="amazing"></Card>
+                <Card name="andy" review="wonderful"></Card>
+                <Card name="kenneth" review="fantastic"></Card>
+                <Card name="michael" review="incredible"></Card>
+              </div>
         </section>
         </>
         )
